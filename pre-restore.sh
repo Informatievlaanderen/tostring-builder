d1707a482c5e4195979542d6f8d992ad6df74f50
jvandaal <jonas.vandaal@outlook.be>
2025-01-29 11:24:39 +0100

    ci: bump build-pipeline

--- pre-restore.sh
+++ pre-restore.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash <(curl -s https://raw.githubusercontent.com/Informatievlaanderen/build-pipeline/refs/heads/main/scripts/pre-restore.sh) 7.0.3
+bash <(curl -s https://raw.githubusercontent.com/Informatievlaanderen/build-pipeline/refs/heads/main/scripts/pre-restore.sh) 7.3.0
